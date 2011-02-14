4185c937b11ab756214c48ea201e9fb46e0160af
Florian Müller <fmui@apache.org>
2011-02-14 12:29:12 +0000

    - changed documentation build parameters
    git-svn-id: https://svn.apache.org/repos/asf/incubator/chemistry/dotcmis/trunk@1070470 13f79535-47bb-0310-9956-ffa450edef68

--- DotCMIS/build.bat
+++ DotCMIS/build.bat
@@ -1,3 +1,3 @@
-msbuild DotCMIS.csproj /p:Configuration=Debug
-msbuild DotCMIS.csproj /p:Configuration=Release
-msbuild DotCMIS.shfbproj /p:Configuration=Release
+msbuild DotCMIS.csproj /ToolsVersion:3.5 /p:Configuration=Debug
+msbuild DotCMIS.csproj /ToolsVersion:3.5 /p:Configuration=Release
+msbuild DotCMIS.shfbproj /ToolsVersion:3.5 /p:Configuration=Release
